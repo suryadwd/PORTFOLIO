54b6d27b4991fdc3376c947e577f05f21a58312c
Suryakant <surajdwivedi644@gmail.com>
2025-02-24 18:56:02 +0530

    deploy

--- app/page.jsx
+++ app/page.jsx
@@ -17,10 +17,14 @@ const Home = () => {
             <p className="max-w-[500px] mb-9 text-white/70">I excel in crafting elegant digital experiences and I am proficient in various programming language and technologies.</p>
 
             <div className="flex flex-col xl:flex-row items-center gap-8">
+              
+              <a href="/CV.pdf.jpg" download={CV.pdf.jpg}>
               <Button variant="outline" size="lg" className="hover:bg-accent transition-all ease-in-out duration-75 hover:text-black  rounded-xl flex items-center gap-2">
                 <span>Download CV</span>
                 <FiDownload  className="text-xl"/>
               </Button>
+              </a>
+
               <div className="mb-8 xl:mb-0">
                 <Socials containerStyles="flex items-center justify-between  gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
               </div>
